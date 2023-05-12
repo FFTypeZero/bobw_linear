fcea8595e65b05325dd62e8565564a59520203c7
Zhihan Xiong <zxiong9@stanford.edu>
2023-05-12 16:20:55 -0700

    more plot code

--- run_adv_multi.py
+++ run_adv_multi.py
@@ -9,7 +9,7 @@ def get_sto_multi(D, T, num_sparse=None):
     Multivariate testing example from Fiez et al. (2019)
     """
     alpha1 = 1
-    alpha2 = 0.5
+    alpha2 = 0.2
     variants = list(range(D))
     individual_index_dict = {}
 
@@ -72,28 +72,18 @@ def get_sto_multi(D, T, num_sparse=None):
     return X, thetas
 
 
-def add_perturbation_multi(X, theta_stars, osci_mag, move_gap, gap_plus, damped):
+def add_perturbation_multi(X, theta_stars, osci_mag, move_gap, gap_plus):
     T, num_features = theta_stars.shape
     theta_star = theta_stars[0]
 
-    damping_length = T / 2
     ts = np.arange(T)
     max_mag = np.max(np.abs(theta_star))
 
     gap, opt_arm = compute_gap(X, theta_star)
     thetas = np.zeros((T, num_features))
     for j in range(num_features):
-        move_gap = np.random.randint(move_gap, move_gap + gap_plus)
-        if j // 2 == 0:
-            if damped:
-                thetas[:, j] = osci_mag * max_mag * np.exp(-ts / damping_length) * np.sin(2 * ts * np.pi / move_gap) + theta_star[j]
-            else:
-                thetas[:, j] = osci_mag * max_mag * np.sin(2 * ts * np.pi / move_gap) + theta_star[j]
-        else:
-            if damped:
-                thetas[:, j] = osci_mag * max_mag * np.exp(-ts / damping_length) * np.cos(2 * ts * np.pi / move_gap) + theta_star[j]
-            else:
-                thetas[:, j] = osci_mag * max_mag * np.cos(2 * ts * np.pi / move_gap) + theta_star[j]
+        phase_shift = np.random.uniform(0, 2 * np.pi)
+        thetas[:, j] = osci_mag * max_mag * np.sin(2 * ts * np.pi / move_gap + phase_shift) + theta_star[j]
 
     gap_adv, opt_arm_adv = compute_gap(X, thetas)
     assert opt_arm_adv == opt_arm
@@ -105,29 +95,30 @@ def run_change_osci(algos, n_trials=1000):
     T = 10000
     noise_level = 0.3
     osci_mags = [1.0 * i for i in range(9)]
+    min_gaps = np.zeros(len(osci_mags))
 
     move_gap = 200
-    gap_plus = 100
-    damped = False
     D = 4
 
-    # algos = ['G_design', 'Peace', 'P1-Peace']
     results_total = np.zeros((len(algos), len(osci_mags), n_trials))
     np.random.seed(6)
     X, theta_stars = get_sto_multi(D, T)
 
     for i, osci_mag in enumerate(osci_mags):
-        thetas = add_perturbation_multi(X, theta_stars, osci_mag, move_gap, gap_plus, damped)
+        thetas = add_perturbation_multi(X, theta_stars, osci_mag, move_gap)
 
         gap, opt_arm = compute_gap(X, thetas)
+        min_gaps[i] = gap
 
         for j, algo in enumerate(algos):
             results = run_trials_in_parallel(n_trials, X, T, thetas, opt_arm, algo, noise_level, 6)
             results_total[j][i] = np.array(results)
-            # np.savez_compressed(f'plot_data/{algo}/{algo}_results_multi_adv9.npz', results=results_total[j], osci_mags=osci_mags)
+            # np.savez_compressed(f'plot_data/{algo}/{algo}_results_multi_osci.npz', 
+                                # results=results_total[j], osci_mags=osci_mags, min_gaps=min_gaps)
 
-    for j, algo in enumerate(algos):
-        print(f"{algo} Accuracy: {np.mean(results_total[j], axis=1)}")
+    # for j, algo in enumerate(algos):
+        # print(f"{algo} Accuracy: {np.mean(results_total[j], axis=1)}")
+    return results_total, min_gaps
 
 
 def run_change_period(algos, n_trials=1000):
@@ -136,68 +127,85 @@ def run_change_period(algos, n_trials=1000):
     osci_mag = 2.0
 
     move_gaps = [300 + 300 * i for i in range(1, 10)]
-    gap_plus = 200
-    damped = False
+    min_gaps = np.zeros(len(move_gaps))
     D = 4
 
-    # algos = ['G_design', 'Peace', 'P1-Peace']
     results_total = np.zeros((len(algos), len(move_gaps), n_trials))
     np.random.seed(6)
     X, theta_stars = get_sto_multi(D, T)
 
     for i, move_gap in enumerate(move_gaps):
-        thetas = add_perturbation_multi(X, theta_stars, osci_mag, move_gap, gap_plus, damped)
+        thetas = add_perturbation_multi(X, theta_stars, osci_mag, move_gap)
 
         gap, opt_arm = compute_gap(X, thetas)
+        min_gaps[i] = gap
 
         for j, algo in enumerate(algos):
             results = run_trials_in_parallel(n_trials, X, T, thetas, opt_arm, algo, noise_level, 6)
             results_total[j][i] = np.array(results)
-            # np.savez_compressed(f'plot_data/{algo}/{algo}_results_multi_adv9.npz', results=results_total[j], move_gaps=move_gaps)
+            # np.savez_compressed(f'plot_data/{algo}/{algo}_results_multi_period.npz', 
+                                # results=results_total[j], move_gaps=move_gaps, min_gaps=min_gaps)
 
-    for j, algo in enumerate(algos):
-        print(f"{algo} Accuracy: {np.mean(results_total[j], axis=1)}")
+    # for j, algo in enumerate(algos):
+        # print(f"{algo} Accuracy: {np.mean(results_total[j], axis=1)}")
+    return results_total, min_gaps
 
 
 def get_plot():
     algos = ['G-BAI', 'Peace', 'P1-Peace']
-    fig, axs = plt.subplots(1, 2)
+    fig, axs = plt.subplots(2, 2)
     for algo in algos:
-        loaded_osci = np.load(f'plot_data/{algo}/{algo}_results_multi_adv8.npz')
-        loaded_period = np.load(f'plot_data/{algo}/{algo}_results_multi_adv9.npz')
+        loaded_osci = np.load(f'plot_data/{algo}/{algo}_results_multi_osci.npz')
+        loaded_period = np.load(f'plot_data/{algo}/{algo}_results_multi_period.npz')
         results_osci = loaded_osci['results']
         results_period = loaded_period['results']
         osci_mags = loaded_osci['osci_mags']
         move_gaps = loaded_period['move_gaps']
+        min_gaps_osci = loaded_osci['min_gaps']
+        min_gaps_period = loaded_period['min_gaps']
 
         error_prob_osci = 1.0 - np.mean(results_osci, axis=1)
         error_prob_period = 1.0 - np.mean(results_period, axis=1)
         confi_bound_osci = 1.96 * np.std(results_osci, axis=1) / np.sqrt(results_osci.shape[1])
         confi_bound_period = 1.96 * np.std(results_period, axis=1) / np.sqrt(results_period.shape[1])
 
-        axs[0].plot(osci_mags, error_prob_osci, 'o-', label=algo)
-        axs[0].fill_between(osci_mags, error_prob_osci - confi_bound_osci, error_prob_osci + confi_bound_osci, alpha=0.4)
-        axs[1].plot(move_gaps, error_prob_period, 'o-', label=algo)
-        axs[1].fill_between(move_gaps, error_prob_period - confi_bound_period, error_prob_period + confi_bound_period, alpha=0.4)
-    axs[0].set_xlabel('oscillation scale')
-    axs[0].set_ylabel('error probability')
-    axs[1].set_xlabel('oscillation period')
-    axs[0].set_title('Error Probability vs. Oscillation Scale')
-    axs[1].set_title('Error Probability vs. Oscillation Period')
-    for i in range(len(axs)):
-        axs[i].set_ylim([-0.05, 1.05])
-        axs[i].legend(loc='best')
-        axs[i].grid(True)
-        axs[i].set_aspect(1.0 / axs[i].get_data_ratio(), adjustable='box')
+        axs[0, 0].plot(osci_mags, error_prob_osci, 'o-', label=algo)
+        axs[0, 0].fill_between(osci_mags, error_prob_osci - confi_bound_osci, error_prob_osci + confi_bound_osci, alpha=0.4)
+        axs[0, 1].plot(move_gaps, error_prob_period, 'o-', label=algo)
+        axs[0, 1].fill_between(move_gaps, error_prob_period - confi_bound_period, error_prob_period + confi_bound_period, alpha=0.4)
+        axs[1, 0].plot(osci_mags, min_gaps_osci, 'o-')
+        axs[1, 1].plot(move_gaps, min_gaps_period, 'o-')
+    axs[1, 0].set_xlabel('oscillation scale')
+    axs[1, 1].set_xlabel('oscillation period')
+    axs[0, 0].set_ylabel('error probability')
+    axs[1, 0].set_ylabel('minimum gap')
+    axs[0, 0].set_title('Error Probability vs. Oscillation Scale')
+    axs[0, 1].set_title('Error Probability vs. Oscillation Period')
+    axs[1, 0].set_title('Minimum Gap vs. Oscillation Scale')
+    axs[1, 1].set_title('Minimum Gap vs. Oscillation Period')
+    for i in range(2):
+        axs[0, i].set_ylim([-0.05, 1.05])
+        axs[0, i].legend(loc='best')
+        axs[0, i].grid(True)
+        # axs[0, i].set_aspect(1.0 / axs[i].get_data_ratio(), adjustable='box')
+    axs[1, 0].grid(True)
+    axs[1, 1].grid(True)
 
     plt.suptitle("Experiments under Non-stationary Multivariate Testing Example")
     plt.show()
 
 
 if __name__ == '__main__':
-    # n_trials = 1000
-    # algos = ['G-BAI', 'Peace', 'P1-Peace']
-    # run_change_osci(algos, n_trials)
-    # run_change_period(algos, n_trials)
+    n_trials = 1000
+    algos = ['G-BAI', 'Peace', 'P1-Peace']
+
+    results_osci, min_gaps_osci = run_change_osci(algos, n_trials)
+    results_period, min_gaps_period = run_change_period(algos, n_trials)
+    for j, algo in enumerate(algos):
+        print(f"{algo} Oscillation magnitude accuracy: {np.mean(results_osci[j], axis=1)}")
+    print(f"Oscillation magnitude minimum gaps: {min_gaps_osci}")
+    for j, algo in enumerate(algos):
+        print(f"{algo} Oscillation period accuracy: {np.mean(results_period[j], axis=1)}")
+    print(f"Oscillation period minimum gaps: {min_gaps_period}")
 
     get_plot()
--- run_adv_soare.py
+++ run_adv_soare.py
@@ -28,8 +28,8 @@ def run_change_osci(algos, n_trials=1000):
     T = 10000
     noise_level = 1.0
     osci_mags = [2.0 * i for i in range(9)]
+    min_gaps = np.zeros(len(osci_mags))
 
-    # algos = ['G_design', 'Peace', 'P1-Peace']
     results_total = np.zeros((len(algos), len(osci_mags), n_trials))
     np.random.seed(6)
 
@@ -37,14 +37,17 @@ def run_change_osci(algos, n_trials=1000):
         X, thetas = get_adv_Soare_1(d, T, omega, osci_mag)
 
         gap, opt_arm = compute_gap(X, thetas)
+        min_gaps[i] = gap
 
         for j, algo in enumerate(algos):
             results = run_trials_in_parallel(n_trials, X, T, thetas, opt_arm, algo, noise_level, 6)
             results_total[j][i] = np.array(results)
-            # np.savez_compressed(f'plot_data/{algo}/{algo}_results_multi_adv9.npz', results=results_total[j], move_gaps=move_gaps)
+            # np.savez_compressed(f'plot_data/{algo}/{algo}_results_soare_osci.npz', 
+                                # results=results_total[j], osci_mags=osci_mags, min_gaps=min_gaps)
 
-    for j, algo in enumerate(algos):
-        print(f"{algo} Accuracy: {np.mean(results_total[j], axis=1)}")
+    # for j, algo in enumerate(algos):
+        # print(f"{algo} Accuracy: {np.mean(results_total[j], axis=1)}")
+    return results_total, min_gaps
 
 
 def get_adv_Soare_2(d, T, omega, move_gap, osci_mag=1.0):
@@ -71,8 +74,8 @@ def run_change_period(algos, n_trials=1000):
     T = 10000
     noise_level = 1.0
     move_gaps = [400 + 400 * i for i in range(1, 10)]
+    min_gaps = np.zeros(len(move_gaps))
 
-    # algos = ['G_design', 'Peace', 'P1-Peace']
     results_total = np.zeros((len(algos), len(move_gaps), n_trials))
     np.random.seed(6)
 
@@ -80,54 +83,74 @@ def run_change_period(algos, n_trials=1000):
         X, thetas = get_adv_Soare_2(d, T, omega, move_gap)
 
         gap, opt_arm = compute_gap(X, thetas)
+        min_gaps[i] = gap
 
         for j, algo in enumerate(algos):
             results = run_trials_in_parallel(n_trials, X, T, thetas, opt_arm, algo, noise_level, 6)
             results_total[j][i] = np.array(results)
-            # np.savez_compressed(f'plot_data/{algo}/{algo}_results_multi_adv9.npz', results=results_total[j], move_gaps=move_gaps)
+            # np.savez_compressed(f'plot_data/{algo}/{algo}_results_soare_period.npz', 
+                                # results=results_total[j], move_gaps=move_gaps, min_gaps=min_gaps)
 
-    for j, algo in enumerate(algos):
-        print(f"{algo} Accuracy: {np.mean(results_total[j], axis=1)}")
+    # for j, algo in enumerate(algos):
+        # print(f"{algo} Accuracy: {np.mean(results_total[j], axis=1)}")
+    return results_total, min_gaps
 
 
 def get_plot():
     algos = ['G-BAI', 'Peace', 'P1-Peace']
     fig, axs = plt.subplots(1, 2)
     for algo in algos:
-        loaded_osci = np.load(f'plot_data/{algo}/{algo}_results_omega0.5_adv6.npz')
-        loaded_period = np.load(f'plot_data/{algo}/{algo}_results_omega0.5_adv7.npz')
+        loaded_osci = np.load(f'plot_data/{algo}/{algo}_results_soare_osci.npz')
+        loaded_period = np.load(f'plot_data/{algo}/{algo}_results_soare_period.npz')
         results_osci = loaded_osci['results']
         results_period = loaded_period['results']
         osci_mags = loaded_osci['osci_mags']
         move_gaps = loaded_period['move_gaps']
+        min_gaps_osci = loaded_osci['min_gaps']
+        min_gaps_period = loaded_period['min_gaps']
 
         error_prob_osci = 1.0 - np.mean(results_osci, axis=1)
         error_prob_period = 1.0 - np.mean(results_period, axis=1)
         confi_bound_osci = 1.96 * np.std(results_osci, axis=1) / np.sqrt(results_osci.shape[1])
         confi_bound_period = 1.96 * np.std(results_period, axis=1) / np.sqrt(results_period.shape[1])
 
-        axs[0].plot(osci_mags, error_prob_osci, 'o-', label=algo)
-        axs[0].fill_between(osci_mags, error_prob_osci - confi_bound_osci, error_prob_osci + confi_bound_osci, alpha=0.4)
-        axs[1].plot(move_gaps, error_prob_period, 'o-', label=algo)
-        axs[1].fill_between(move_gaps, error_prob_period - confi_bound_period, error_prob_period + confi_bound_period, alpha=0.4)
-    axs[0].set_xlabel('oscillation magnitude')
-    axs[0].set_ylabel('error probability')
-    axs[1].set_xlabel('oscillation period')
-    axs[0].set_title('Error Probability vs. Oscillation Magnitude')
-    axs[1].set_title('Error Probability vs. Oscillation Period')
-    for i in range(len(axs)):
-        axs[i].set_ylim([-0.05, 1.05])
-        axs[i].legend(loc='best')
-        axs[i].grid(True)
-        axs[i].set_aspect(1.0 / axs[i].get_data_ratio(), adjustable='box')
+        axs[0, 0].plot(osci_mags, error_prob_osci, 'o-', label=algo)
+        axs[0, 0].fill_between(osci_mags, error_prob_osci - confi_bound_osci, error_prob_osci + confi_bound_osci, alpha=0.4)
+        axs[0, 1].plot(move_gaps, error_prob_period, 'o-', label=algo)
+        axs[0, 1].fill_between(move_gaps, error_prob_period - confi_bound_period, error_prob_period + confi_bound_period, alpha=0.4)
+        axs[1, 0].plot(osci_mags, min_gaps_osci, 'o-')
+        axs[1, 1].plot(move_gaps, min_gaps_period, 'o-')
+    axs[1, 0].set_xlabel('oscillation scale')
+    axs[1, 1].set_xlabel('oscillation period')
+    axs[0, 0].set_ylabel('error probability')
+    axs[1, 0].set_ylabel('minimum gap')
+    axs[0, 0].set_title('Error Probability vs. Oscillation Scale')
+    axs[0, 1].set_title('Error Probability vs. Oscillation Period')
+    axs[1, 0].set_title('Minimum Gap vs. Oscillation Scale')
+    axs[1, 1].set_title('Minimum Gap vs. Oscillation Period')
+    for i in range(2):
+        axs[0, i].set_ylim([-0.05, 1.05])
+        axs[0, i].legend(loc='best')
+        axs[0, i].grid(True)
+        # axs[0, i].set_aspect(1.0 / axs[i].get_data_ratio(), adjustable='box')
+    axs[1, 0].grid(True)
+    axs[1, 1].grid(True)
+
     plt.suptitle("Experiments under Non-stationary Soare et al. (2014)'s Example")
     plt.show()
 
 
 if __name__ == '__main__':
-    # n_trials = 1000
-    # algos = ['G-BAI', 'Peace', 'P1-Peace']
-    # run_change_osci(algos, n_trials)
-    # run_change_period(algos, n_trials)
+    n_trials = 1000
+    algos = ['G-BAI', 'Peace', 'P1-Peace']
+
+    results_osci, min_gaps_osci = run_change_osci(algos, n_trials)
+    results_period, min_gaps_period = run_change_period(algos, n_trials)
+    for j, algo in enumerate(algos):
+        print(f"{algo} Oscillation magnitude accuracy: {np.mean(results_osci[j], axis=1)}")
+    print(f"Oscillation magnitude minimum gaps: {min_gaps_osci}")
+    for j, algo in enumerate(algos):
+        print(f"{algo} Oscillation period accuracy: {np.mean(results_period[j], axis=1)}")
+    print(f"Oscillation period minimum gaps: {min_gaps_period}")
 
     get_plot()
